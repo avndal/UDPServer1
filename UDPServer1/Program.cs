6b37a3887d5be44d17c5856ea2e4746a0c24faa9
Rasmus Avndal Hansen <rah001@edu.zealand.dk>
2024-09-12 10:52:57 +0200

    Add project files.

---
+++ UDPServer1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+Console.WriteLine("UDP Server");
+
+using (UdpClient socket = new UdpClient())
+{
+    socket.Client.Bind(new IPEndPoint(IPAddress.Any,5005));
+    
+    IPEndPoint clientEndpoint = null;
+    byte[] received = socket.Receive(ref clientEndpoint);
+    
+    string stringResult = Encoding.UTF8.GetString(received);
+
+    Console.WriteLine(stringResult);
+}
+
+
+
